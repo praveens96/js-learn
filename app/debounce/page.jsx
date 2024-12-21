5b3bdb3bc5a44a1cec502458e369744b069c7c5f
praveens96 <praveen13400@gmail.com>
2024-12-21 12:32:22 +0530

    chore: enhanced debounce (#1)
    Co-authored-by: Praveen Seela <praveen.seela@advance-auto.com>

--- app/debounce/page.jsx
+++ app/debounce/page.jsx
@@ -1,35 +1,53 @@
 "use client";
 
+import { useRef } from "react";
+import { useState } from "react";
 import { useCallback } from "react";
 
 const debounced = (mainFn, delay) => {
   let timer;
+  let callCount = 0;
   return function (...args) {
     try {
       clearTimeout(timer);
       timer = setTimeout(() => {
-        mainFn(...args);
+        mainFn(...args, this.callCount++);
       }, delay);
     } catch (ex) {
       console.error(ex);
     }
   };
 };
-const someFn = () => {
-  console.log("on click debounce");
+
+const useDebounced = (mainFn, delay) => {
+  const timer = useRef();
+  const callCount = useRef(0);
+
+  return function (...args) {
+    clearTimeout(timer.current);
+    timer.current = setTimeout(() => {
+      mainFn(...args, ++callCount.current);
+    }, delay);
+  };
 };
+
+const someFn = (count, callCount) => {
+  console.log("on click debounce", count, callCount);
+};
+
 export default function First() {
-  const debouncedFn = useCallback(
-    debounced(someFn, 500)
-  , [])
+  const [state, setState] = useState(0);
+  const debouncedFn = useCallback(useDebounced(someFn, 500), []);
+
   return (
     <div>
       <p>first</p>
       <button
         onClick={() => {
           // console.log("onclik");
+          setState((state) => state + 1);
           try {
-            debouncedFn();
+            debouncedFn(state);
           } catch (error) {
             console.error("onclick err", error);
           }
